d43903fcc31b79357e6693a57c60cf4967f47726
David Tawiah <57331395+tawiah007@users.noreply.github.com>
2024-03-05 23:02:19 +0000

    Login and Register Auth Successfully added

--- app.py
+++ app.py
@@ -1,14 +1,83 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 import csv
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# CREATE TABLE IN DB
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/authentication')
+def auth():
+    return render_template("auth.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        data = request.form
+        email = data['email']
+        # Check if a user with the same email already exists
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash("You've already signed up with that email, please login instead.")
+            return render_template("login.html")
+        # Hash and salt the password
+        hashed_password = generate_password_hash(data['password'], salt_length=8)
+        new_user = User(
+            email=email,
+            password=hashed_password,
+            name=data['name']
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('hostel_sections'))
+    return render_template("register.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        # Email doesn't exist or password incorrect.
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('hostel_sections'))
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 @app.route('/hostel-sections', methods=["GET", "POST"])
 def hostel_sections():
